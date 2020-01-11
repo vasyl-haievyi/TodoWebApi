df285c728baa794fea9a0f12c1364fb3d6175b21
pesto12 <vasyl.gayevyj@gmail.com>
2020-01-11 12:16:56 +0100

    Implemented topicController

--- TodoWebApi/Controllers/TopicController.cs
+++ TodoWebApi/Controllers/TopicController.cs
@@ -6,40 +6,58 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TodoWebApi.Dtos;
+using TodoWebApi.Services;
 
 namespace TodoWebApi.Controllers {
+
     [ApiController]
     [Route("topics/")]
     public class TopicController : Controller {
 
+        private readonly ITopicService _service;
+
+        public TopicController(ITopicService service) {
+            _service = service;
+        }
+
         [HttpGet]
-        private Task<ActionResult> GetAllTopics() {
-            throw new NotImplementedException();
+        private async Task<ActionResult> GetAllTopics() {
+            var result = await _service.GetAllTopicsHandler();
+
+            return Ok(result); 
         }
 
         [HttpGet]
         [Route("{Id}")]
-        private Task<ActionResult> GetTopic( [FromRoute][Required] Guid Id) {
-            throw new NotImplementedException();
+        private async Task<ActionResult> GetTopic( [FromRoute][Required] Guid Id) {
+            var result = await _service.GetTopicHandler(Id);
+
+            return Ok(result);
         }
 
         [HttpPost]
-        private Task<ActionResult> CreateTopic([FromBody][Required] TopicDto topicDto) {
-            throw new NotImplementedException();
+        private async Task<ActionResult> CreateTopic([FromBody][Required] TopicDto topicDto) {
+            await _service.CreateTopicHandler(topicDto);
+
+            return Ok();
         }
 
         [HttpPut]
         [Route("{Id}")]
-        private Task<ActionResult> UpdateTopic(
+        private async Task<ActionResult> UpdateTopic(
             [FromRoute][Required] Guid Id,
             [FromBody][Required] TopicDto topicDto) {
-            throw new NotImplementedException();
+            await _service.UpdateTopicHandler(Id, topicDto);
+
+            return Ok();
         }
 
         [HttpDelete]
         [Route("Id")]
-        private Task<ActionResult> DeleteTopic([FromRoute][Required] Guid Id) {
-            throw new NotImplementedException();
+        private async Task<ActionResult> DeleteTopic([FromRoute][Required] Guid Id) {
+            await _service.DeleteTopicHandler(Id);
+
+            return Ok();
         }
     }
 }
--- TodoWebApi/Startup.cs
+++ TodoWebApi/Startup.cs
@@ -10,29 +10,26 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TodoWebApi.Services;
+using TodoWebApi.Services.Impl;
 
-namespace TodoWebApi
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace TodoWebApi {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
+            services.AddSingleton<ITopicService, TopicServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -42,8 +39,7 @@ namespace TodoWebApi
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
         }
